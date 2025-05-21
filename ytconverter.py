93d910715c9344f75ca46b3dbfe078049dbff78e
KAIF_CODEC <98528577+kaifcodec@users.noreply.github.com>
2025-05-21 08:15:09 +0530

    UnicodeEncodeError fix windows

--- ytconverter.py
+++ ytconverter.py
@@ -27,10 +27,10 @@ import traceback
 from pathlib import Path
 import sys
 
- try:
-     sys.stdout.reconfigure(encoding='utf-8')
- except AttributeError:
-     pass
+try:
+    sys.stdout.reconfigure(encoding='utf-8')
+except AttributeError:
+    pass
 print("\n" + "Attempting to import required modules".center(99))
 try:
     from colored import fg, attr
